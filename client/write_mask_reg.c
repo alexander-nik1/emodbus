fba5d9e22508750eb8a2c581eef92e7478fbb0d1
alex <alex.northfield@gmail.com>
2016-03-31 17:18:44 +0300

    Доделал функцию "Mask Write Register"

--- client/write_mask_reg.c
+++ client/write_mask_reg.c
@@ -5,29 +5,49 @@
 #include <emodbus/client/write_mask_reg.h>
 #include <string.h>
 
-int write_mask_reg_make_req(void* _result_req, uint8_t _slave_addr,
-                               uint16_t _address, uint16_t _and_mask, uint16_t _or_mask) {
+/*!
+ * \file
+ * \brief The definition of "Mask Write Register" functions.
+ *
+ * Functions for working with "Mask Write Register" function.
+ *
+ */
 
-    ((uint8_t*)_result_req)[0] = _slave_addr;
-    ((uint8_t*)_result_req)[1] = 0x16;
-    ((uint16_t*)_result_req)[1] = SWAP_BYTES(_address);
-    ((uint16_t*)_result_req)[2] = SWAP_BYTES(_and_mask);
-    ((uint16_t*)_result_req)[3] = SWAP_BYTES(_or_mask);
-    return 8;
+int emb_write_mask_reg_calc_req_data_size() {
+    return 6;
 }
 
-int write_mask_reg_valid_answer(const void* _req, unsigned int _req_size,
-                                   const void* _answer, unsigned int _answer_size) {
-    int r;
+int emb_write_mask_reg_calc_answer_data_size() {
+    return 6;
+}
 
-//    if((r = modbus_check_answer(_req, _answer)) != 0)
-//        return r;
+int emb_write_mask_reg_make_req(emb_pdu_t* _result_req,
+                                uint16_t _address,
+                                uint16_t _and_mask,
+                                uint16_t _or_mask) {
 
-    if(_req_size != 8)
-        return -EINVAL;
+    if(_result_req->max_size < emb_write_mask_reg_calc_req_data_size()) {
+        return -ENOMEM;
+    }
 
-    if(_req_size != _answer_size)
-        return -1;
+    _result_req->data_size = 6;
+    _result_req->function = 0x16;
 
-    return memcmp(_req, _answer, _req_size);
+    ((uint16_t*)_result_req->data)[0] = SWAP_BYTES(_address);
+    ((uint16_t*)_result_req->data)[1] = SWAP_BYTES(_and_mask);
+    ((uint16_t*)_result_req->data)[2] = SWAP_BYTES(_or_mask);
+
+    return 0;
+}
+
+uint16_t emb_write_mask_reg_get_address(emb_const_pdu_t* _pdu) {
+    return ((uint16_t*)_pdu->data)[0];
+}
+
+uint16_t emb_write_mask_reg_get_and_mask(emb_const_pdu_t* _pdu) {
+    return ((uint16_t*)_pdu->data)[1];
+}
+
+uint16_t emb_write_mask_reg_get_or_mask(emb_const_pdu_t* _pdu) {
+    return ((uint16_t*)_pdu->data)[2];
 }
--- include/emodbus/client/write_mask_reg.h
+++ include/emodbus/client/write_mask_reg.h
@@ -4,17 +4,82 @@
 
 #include <stdint.h>
 
+/*!
+ * \file
+ * \brief The dclaration of "Mask Write Register" functions.
+ *
+ * Functions for working with "Mask Write Register" function.
+ *
+ */
+
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-// Build request
-int write_mask_reg_make_req(void* _result_req, uint8_t _slave_addr,
-                               uint16_t _address, uint16_t _and_mask, uint16_t _or_mask);
+/**
+ * @brief Calculate the request size
+ * @return The size of request's data size
+ */
+int emb_write_mask_reg_calc_req_data_size();
 
-// Check and valid answer
-int write_mask_reg_valid_answer(const void* _req, unsigned int _req_size,
-                                   const void* _answer, unsigned int _answer_size);
+/**
+ * @brief Calculate the answer size
+ * @return The size of answer's data size
+ */
+int emb_write_mask_reg_calc_answer_data_size();
+
+/**
+ * @brief Build request
+ *
+ * This fucntion builds the "Mask Write Register" request.
+ *
+ * \param[out] _result_req Result request.
+ * \param[in] _result_req Starting address of holding register.
+ * \param[in] _and_mask AND mask
+ * \param[in] _or_mask OR mask
+ * @return Zero if a request is ready, otherwise error code.
+ */
+int emb_write_mask_reg_make_req(emb_pdu_t* _result_req,
+                                uint16_t _address,
+                                uint16_t _and_mask,
+                                uint16_t _or_mask);
+
+/**
+ * @brief Get address
+ *
+ * This function returns address, which got from _pdu parameter.
+ * Because, an answer is a echo of the request, _pdu can be any:
+ * request or answer.
+ *
+ * @param[in] _pdu Can be an answer or a request for this modbus-function.
+ * @return Starting address of holding register.
+ */
+uint16_t emb_write_mask_reg_get_address(emb_const_pdu_t* _pdu);
+
+/**
+ * @brief Get AND mask
+ *
+ * This function returns AND amsk, which got from _pdu parameter.
+ * Because, an answer is a echo of the request, _pdu can be any:
+ * request or answer.
+ *
+ * @param[in] _pdu Can be an answer or a request for this modbus-function.
+ * @return AND mask
+ */
+uint16_t emb_write_mask_reg_get_and_mask(emb_const_pdu_t* _pdu);
+
+/**
+ * @brief Get OR mask
+ *
+ * This function returns OR amsk, which got from _pdu parameter.
+ * Because, an answer is a echo of the request, _pdu can be any:
+ * request or answer.
+ *
+ * @param[in] _pdu Can be an answer or a request for this modbus-function.
+ * @return OR mask
+ */
+uint16_t emb_write_mask_reg_get_or_mask(emb_const_pdu_t* _pdu);
+
 
 #ifdef __cplusplus
 }   // extern "C"
