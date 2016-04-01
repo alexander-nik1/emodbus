b14621d5343a1ad1ca583f2921ffb9d27dbe0a52
alex <alex.northfield@gmail.com>
2016-04-01 15:03:19 +0300

    I had made a Read FIFO function.

--- client/read_fifo.c
+++ client/read_fifo.c
@@ -1,3 +1,62 @@
 
 #include <emodbus/client/read_fifo.h>
+#include <errno.h>
+#include <emodbus/base/byte-word.h>
 
+/*!
+ * \file
+ * \brief Definition of Read Fifo functions.
+ *
+ * Functions for working with Read Fifo function.
+ *
+ */
+
+int emb_read_fifo_calc_req_data_size() {
+    return 2;
+}
+
+int emb_read_fifo_calc_answer_data_size() {
+    return 2 + 2 + 2 * 31;
+}
+
+int emb_read_fifo_make_req(emb_pdu_t* _result_req,
+                           uint16_t _starting_address) {
+
+    const int req_size = emb_read_fifo_calc_req_data_size();
+
+    if(_result_req->max_size < req_size) {
+        return -ENOMEM;
+    }
+
+    _result_req->function = 0x18;
+    _result_req->data_size = req_size;
+
+    ((uint16_t*)_result_req->data)[0] = SWAP_BYTES(_starting_address);
+
+    return 0;
+}
+
+uint16_t emb_read_fifo_byte_count(emb_const_pdu_t* _answer) {
+    if(_answer) {
+        const uint16_t x = ((uint16_t*)_answer->data)[0];
+        return SWAP_BYTES(x);
+    }
+    return -1;
+}
+
+uint16_t emb_read_fifo_regs_count(emb_const_pdu_t* _answer) {
+    if(_answer) {
+        const uint16_t x = ((uint16_t*)_answer->data)[1];
+        return SWAP_BYTES(x);
+    }
+    return -1;
+}
+
+uint16_t emb_read_fifo_get_data(emb_const_pdu_t* _answer,
+                                uint16_t _offset) {
+    if(_answer && _offset < emb_read_fifo_regs_count(_answer)) {
+        const uint16_t x = ((uint16_t*)_answer->data)[2 + _offset];
+        return SWAP_BYTES(x);
+    }
+    return -1;
+}
--- include/emodbus/client/read_fifo.h
+++ include/emodbus/client/read_fifo.h
@@ -3,5 +3,82 @@
 #define MODBUS_MASTER_READ_FIFO
 
 
+/*!
+ * \file
+ * \brief Declaration of Read Fifo functions.
+ *
+ * Functions for working with Read Fifo function.
+ *
+ */
+
+#include <emodbus/base/modbus_pdu.h>
+#include <stdint.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/**
+ * @brief Calculate the request size
+ * @return The size of request's data size
+ */
+int emb_read_fifo_calc_req_data_size();
+
+/**
+ * @brief Calculate the answer size
+ * @return The size of answer's data size
+ */
+int emb_read_fifo_calc_answer_data_size();
+
+/**
+ * @brief Build request
+ *
+ * This fucntion builds the "Read Fifo" request.
+ *
+ * @param[out] _result_req Result request.
+ * @param[in] _starting_address Address of starting register
+ * @return Zero if a request is ready, otherwise error code.
+ */
+int emb_read_fifo_make_req(emb_pdu_t* _result_req,
+                           uint16_t _starting_address);
+
+/**
+ * @brief Get byte count from answer
+ *
+ * This function can retrun a byte-count field
+ * from a given answer.
+ *
+ * @param[in] _answer The answer
+ * @return Byte count
+ */
+uint16_t emb_read_fifo_byte_count(emb_const_pdu_t* _answer);
+
+/**
+ * @brief Get regs count from answer
+ *
+ * This function can retrun a registers count field
+ * from a given answer.
+ *
+ * @param[in] _answer The answer
+ * @return Registers count
+ */
+uint16_t emb_read_fifo_regs_count(emb_const_pdu_t* _answer);
+
+/**
+ * @brief Get the data register from answer
+ *
+ * This function can retrun a data register
+ * from a given answer.
+ *
+ * @param[in] _answer The answer
+ * @param[in] _offset The offset to a data within the answer.
+ * @return Data register.
+ */
+uint16_t emb_read_fifo_get_data(emb_const_pdu_t* _answer,
+                                uint16_t _offset);
+
+#ifdef __cplusplus
+}   // extern "C"
+#endif
 
 #endif // MODBUS_MASTER_READ_FIFO
