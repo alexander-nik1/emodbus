a5c97de8f1becdbb16c113033d5e0bcabb5a51b5
alex <alex.northfield@gmail.com>
2016-11-29 13:15:42 +0300

    Remake example to use RTU via TCP-client.

--- examples/client.cpp
+++ examples/client.cpp
@@ -1,10 +1,10 @@
 
 #include <emodbus/impl/posix/client.hpp>
 #include <emodbus/impl/posix/mb-rtu-via-serial.h>
+#include <emodbus/impl/posix/mb-rtu-via-tcp-client.h>
 #include <emodbus/impl/posix/dumper.h>
 #include <emodbus/base/modbus_errno.h>
 
-#include <stdlib.h>
 #include <unistd.h>
 #include <iostream>
 #include <exception>
@@ -29,15 +29,16 @@ void* thr_proc(void* p) {
     struct event_base *base = event_base_new();
 
     // Create the RTU transport
-    struct emb_rtu_via_serial_t* rtu = emb_rtu_via_serial_create(base, 10, "/dev/ttyUSB0", 115200);
+    //struct emb_rtu_via_serial_t* rtu = emb_rtu_via_serial_create(base, 10, "/dev/ttyUSB0", 115200);
+    struct emb_rtu_via_tcp_client_t* rtu = emb_rtu_via_tcp_client_create(base, 40, "192.168.1.138", 4003);
 
     // Enable a packets dumping
-    emb_rtu_via_serial_get_transport(rtu)->flags |= EMB_TRANSPORT_FLAG_DUMD_PAKETS;
+    emb_rtu_via_tcp_client_get_transport(rtu)->flags |= EMB_TRANSPORT_FLAG_DUMD_PAKETS;
     emb_posix_dumping_stream = stdout;
-    emb_posix_dumper_enable_rx_tx();
+    //emb_posix_dumper_enable_rx_tx();
 
     // Bind the client to the RTU
-    client.set_transport(emb_rtu_via_serial_get_transport(rtu));
+    client.set_transport(emb_rtu_via_tcp_client_get_transport(rtu));
 
     // Because, a client was created befire the base,
     // we need init the client's timers here.
@@ -49,7 +50,7 @@ void* thr_proc(void* p) {
     event_base_dispatch(base);
 
     // Destroy the RTU
-    emb_rtu_via_serial_destroy(rtu);
+    emb_rtu_via_tcp_client_destroy(rtu);
 }
 
 /***************************************************************************************************/
@@ -61,8 +62,11 @@ int main() {
     pthread_create(&pthr, NULL, thr_proc, NULL);
     sleep(1);   // We must wait here for all initialization is made in started thread.
 
-    emb::client::proxy_t device1(&client, 16);
-    emb::client::proxy_t device2(&client, 17);
+    emb::client::proxy_t device1(&client, 0x10);
+    emb::client::proxy_t device2(&client, 0x30);
+
+    device1.set_timeout(1000);
+    device2.set_timeout(1000);
 
     for(int i=0; i<1000; ++i) {
 
@@ -85,20 +89,20 @@ int main() {
             std::cout << std::endl;
 
             // Read a 0x0000-0x0007 (eight) registers form server 17
-            regs = device2.holdings[emb::range_t(0x0000, 0x0007)];
+            regs = device2.holdings[emb::range_t(0x0020, 0x0027)];
             std::cout << "REGS2: ";
             for(emb::regs_t::const_iterator i = regs.begin(); i != regs.end(); ++i)
                 std::cout << *i << " ";
             std::cout << std::endl;
         }
         catch(std::exception& e) {
-            printf("Exception: %s\n", e.what());
+            std::cerr << "Exception: " << e.what() << std::endl;
         }
         catch(int & e) {
-            printf("Exception: %d (%s)\n", e, emb_strerror(-e));
+            std::cerr << "Exception: (" << e << ") " << emb_strerror(-e) << std::endl;
         }
     }
 
-    printf("RX bytes: %ld\n", emb_posix_dumper_rx_bytes());
-    printf("TX bytes: %ld\n", emb_posix_dumper_tx_bytes());
+    std::cout << "RX bytes: " << emb_posix_dumper_rx_bytes() << std::endl;
+    std::cout << "TX bytes: " << emb_posix_dumper_tx_bytes() << std::endl;
 }
