144e310d5ee649e98700da85dfca7b0ff606e1cf
Alexander-nik1 <alex.northfield@gmail.com>
2019-11-14 00:11:49 +0300

    Doxy-comments for bits.h

--- emodbus/include/emodbus/server/bits.h
+++ emodbus/include/emodbus/server/bits.h
@@ -8,16 +8,66 @@
 extern "C" {
 #endif
 
-struct emb_srv_bits_t {
+/*!
+ * \file
+ * \brief Server, bits definitions
+ *
+ */
 
-    uint16_t start;
-    uint16_t size;
+/**
+ * @brief Bits range
+ *
+ * A range to represent a modbus-coils and
+ * modbus-discrete-inputs.
+ *
+ */
+struct emb_srv_bits_t
+{
+    uint16_t start; ///< Begining address of bits range
+    uint16_t size;  ///< Nuber of bits in the range
 
+    /**
+     * @brief Read bits
+     *
+     * This is a user-defined function.
+     * Function called by a emb_srv_read_bits function to
+     * request bits from user.
+     *
+     * @param [in] _coils The bits-range (coils or discrete-inputs)
+     * @param [in] _offset Offset inside this range, to begin reading from
+     * @param [in] _quantity Number of coils to read
+     * @param [out] _pvalues A place for write bits into
+     *
+     * @return If 0, then all is ok,
+     * otherwise, an modbus-exception packet will be sent, error-code will be taken
+     * from this returning value.
+     *
+     * @see emb_srv_read_bits
+     */
     uint8_t (*read_bits)(struct emb_srv_bits_t* _coils,
                           uint16_t _offset,
                           uint16_t _quantity,
                           uint8_t* _pvalues);
 
+    /**
+     * @brief Write bits
+     *
+     * This is a user-defined function.
+     * Function called by a emb_srv_write_coil and emb_srv_write_coils functions to
+     * send bits to user.
+     *
+     * @param [in] _coils The coils-range
+     * @param [in] _offset Offset inside this range, to begin write from
+     * @param [in] _quantity Number of coils to write
+     * @param [in] _pvalues A place for take bits from
+     *
+     * @return If 0, then all is ok,
+     * otherwise, an modbus-exception packet will be sent, error-code will be taken
+     * from this returning value.
+     *
+     * @see emb_srv_write_coil
+     * @see emb_srv_write_coils
+     */
     uint8_t (*write_bits)(struct emb_srv_bits_t* _coils,
                            uint16_t _offset,
                            uint16_t _quantity,
