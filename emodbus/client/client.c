b82e1796860a565a8ee72babd4d28de74a993a4f
Alexander-nik1 <alex.northfield@gmail.com>
2020-06-26 21:01:50 +0300

    Remake client.

--- emodbus/client/client.c
+++ emodbus/client/client.c
@@ -10,109 +10,20 @@
  *
  */
 
-#define CLIENT_REQ_ON_ERROR(_req_, _addr_, _errno_)        \
-    if((_req_->procs) && (_req_->procs->on_error))         \
-        _req_->procs->on_error(_req_, _addr_, _errno_)
-/*
-static void emb_client_on_receive_pkt(void* _user_data,
-                           int _slave_addr,
-                           emb_const_pdu_t* _pkt) {
-
-    struct emb_client_t* cli = (struct emb_client_t*)_user_data;
-    struct emb_client_transaction_t* req = cli->curr_transaction;
-    int res;
-
-    cli->curr_transaction = (struct emb_client_transaction_t*)0;
-
-    //cli->transport->rx_pdu = NULL;
-
-    do {
-
-        if(!req) {
-            // something wrong ...
-            //printf("emb_client_on_receive_pkt()2\n"); fflush(stdout);
-            break;
-        }
-
-        if(cli->curr_addr != _slave_addr) {
-            if(cli->on_error)
-                cli->on_error(cli, _slave_addr, -modbus_resp_wrong_address);
-            CLIENT_REQ_ON_ERROR(req, _slave_addr, -modbus_resp_wrong_address);
-            break;
-        }
-
-        if((res = emb_check_pdu_for_exception(_pkt))) {
-            if(cli->on_error)
-                cli->on_error(cli, _slave_addr, res);
-            CLIENT_REQ_ON_ERROR(req, _slave_addr, res);
-            break;
-        }
-
-        if(req->req_pdu->function != _pkt->function) {
-            if(cli->on_error)
-                cli->on_error(cli, _slave_addr, -modbus_resp_wrong_func);
-            CLIENT_REQ_ON_ERROR(req, _slave_addr, -modbus_resp_wrong_func);
-            break;
-        }
-
-        if(req->procs && req->procs->on_response)
-            req->procs->on_response(req, _slave_addr);
-
-        if(cli->on_response)
-            cli->on_response(cli, _slave_addr);
+int emb_sync_client_do_request(emb_sync_client_t* _cli, const emb_adu_t* _req_adu, emb_adu_t* _ans_adu)
+{
+    int r;
+    if(!(_cli && _cli->recv_adu && _cli->send_adu && _req_adu && _ans_adu)) {
+        return -modbus_invalid_argument;
     }
-    while(0);
-}
 
-static void emb_client_on_error(void* _user_data, int _errno) {
-    struct emb_client_t* cli = (struct emb_client_t*)_user_data;
-    struct emb_client_transaction_t* req = cli->curr_transaction;
-    if(req) {
-        cli->curr_transaction = (struct emb_client_transaction_t*)0;
-        cli->transport->rx_pdu = (emb_pdu_t*)0;
-        if(cli->on_error)
-            cli->on_error(cli, cli->curr_addr, _errno);
-        CLIENT_REQ_ON_ERROR(req, cli->curr_addr, _errno);
-    }
-}
+    r = _cli->send_adu(_cli, _req_adu);
+    if(r != modbus_success)
+        return r;
 
-void emb_client_init(struct emb_client_t *_cli) {
-    emb_client_set_transport(_cli, _cli->transport);
-    _cli->curr_transaction = (struct emb_client_transaction_t*)0;
-}
+    r = _cli->recv_adu(_cli, _ans_adu);
+    if(r != modbus_success)
+        return r;
 
-void emb_client_wait_timeout(struct emb_client_t* _cli) {
-    struct emb_client_transaction_t* req = _cli->curr_transaction;
-    _cli->transport->rx_pdu = NULL;
-    //        if(_cli->on_error)
-    //            _cli->on_error(_cli, _cli->curr_addr, -modbus_resp_timeout);
-    if(req) {
-        _cli->curr_transaction = (struct emb_client_transaction_t*)0;
-        CLIENT_REQ_ON_ERROR(req, _cli->curr_addr, -modbus_resp_timeout);
-    }
+    return emb_check_pdu_for_exception(&_ans_adu->pdu);
 }
-
-int emb_client_do_transaction(struct emb_client_t* _cli,
-                          int _slave_addr,
-                          struct emb_client_transaction_t* _transact) {
-
-    if(_cli->transport->rx_pdu)
-        return -EBUSY;
-    _cli->curr_addr = _slave_addr;
-    _cli->curr_transaction = _transact;
-    _cli->transport->rx_pdu = _transact->ans_pdu;
-    emb_transport_send_packet(_cli->transport, _slave_addr, _transact->req_pdu);
-    return 0;
-}
-
-void emb_client_set_transport(struct emb_client_t* _cli,
-                              struct emb_transport_t *_transport) {
-    if(_transport) {
-        _cli->transport = _transport;
-        _transport->high_level_context = _cli;
-        _transport->recv_packet = emb_client_on_receive_pkt;
-        _transport->error = emb_client_on_error;
-        _transport->flags &= (~EMB_TRANSPORT_FLAG_IS_SERVER);
-    }
-}
-*/
--- emodbus/include/emodbus/client/client.h
+++ emodbus/include/emodbus/client/client.h
@@ -15,106 +15,30 @@
 extern "C" {
 #endif
 
-struct emb_client_transaction_t;
-struct emb_client_t;
-
-/**
- * @brief The emb_client_req_procs_t struct
- *
- * The callbacks for request.
- */
-struct emb_client_req_procs_t {
-    /// Calls when a correct response was received.
-    void (*on_response)(struct emb_client_transaction_t*,
-                        int _slave_addr);
-
-    /// Calls when happens a some error, like: system error, receiving error(for example, CRC error),
-    /// and modbus error(error, returned by server (slave) device)
-    void (*on_error)(struct emb_client_transaction_t*,
-                     int _slave_addr, int _errno);
-};
-
-/**
- * @brief The emb_client_request_t struct
- *
- * This is a ONE request descriptor for client.
- *
- */
-struct emb_client_transaction_t
-{
-    emb_adu_t* req_pdu;                     ///< Receive ADU
-    emb_adu_t* ans_pdu;                     ///< Answer ADU
-    struct emb_client_req_procs_t* procs;   ///< Callbacks for this request
-    void* user_data;                        ///< Some user data for high-level :)
-};
-
-int emb_client_send_request(struct emb_client_transaction_t* _req);
-
 /**
  * @brief The emb_client_t struct
  *
  * This is a ONE modbus client cntext.
  *
  */
-struct emb_client_t
+typedef struct __emb_sync_client_t
 {
-    /// This variable saves a current-request address
-    int curr_addr;
-
-    /// This variable saves a pointer to a current request.
-    struct emb_client_transaction_t* curr_transaction;
-
-    /// This function calls by client. A call says about a response was received.
-    void (*on_response)(struct emb_client_t* _cli, int _slave_addr);
-
-    /// This function calls by client. A call indicates an error.
-    void (*on_error)(struct emb_client_t* _cli, int _slave_addr, int _errno);
-};
+    int (*send_adu)(struct __emb_sync_client_t* _cli, const emb_adu_t* _adu);
+    int (*recv_adu)(struct __emb_sync_client_t* _cli, emb_adu_t* _adu);
+} emb_sync_client_t;
 
 /**
- * @brief Initialization
- *
- * This function performs a initialization of a client.
- *
- * @param [in] _cli a pointer to client.
- */
-void emb_client_init(struct emb_client_t* _cli);
-
-/**
- * @brief Timeout signal
- *
- * This function must be called by high level context,
- * when a response-waiting time was expired.
- *
- * @param [in] _cli a pointer to client.
- */
-void emb_client_wait_timeout(struct emb_client_t* _cli);
-
-/**
- * @brief Do request
+ * @brief Do sync request
  *
  * This function performs a ONE request.
  *
  * @param [in] _cli a pointer to client.
- * @param [in] _slave_addr address of target server.
- * @param [in] _req request rescription.
+ * @param [in] _req_adu request ADU.
+ * @param [out] _ans_req response ADU.
  * @return if there is no errors, it will return zero, otherwize
  * it will return a error code. You can see it by emb_strerror() function.
  */
-int emb_client_do_transaction(struct emb_client_t* _cli,
-                              int _slave_addr,
-                              struct emb_client_transaction_t* _transact);
-
-/**
- * @brief Set low level
- *
- * This function set's a new low-level context for this client.
- *
- * @param _cli a pointer to client.
- * @param _transport a new transport.
- */
-void emb_client_set_transport(struct emb_client_t* _cli,
-                              struct emb_transport_t* _transport);
+int emb_sync_client_do_request(emb_sync_client_t* _cli, const emb_adu_t* _req_adu, emb_adu_t* _ans_adu);
 
 #ifdef __cplusplus
 }   // extern "C"
